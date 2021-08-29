ebde27f71c9eef23ec2374217fdcf191af6ac1db
Developeramal-new <amalfer0308@gmail.com>
2021-08-29 11:53:55 +0530

    imageslider added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,9 +14,10 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { ImageSliderComponent } from './shared/image-slider/image-slider.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HeaderDirective, HomeComponent],
+  declarations: [AppComponent, HeaderComponent, HeaderDirective, HomeComponent, ImageSliderComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
